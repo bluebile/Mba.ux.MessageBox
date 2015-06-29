bb60392a7ddcd624d0fb01b4632a9ad8be3eb872
ramonornela <ramon.ornela@gmail.com>
2015-06-29 09:32:06 -0300

    [UPDATE]
    - Commit inicial

---
+++ MessageBox.js
@@ -0,0 +1,23 @@
+Ext.define('Mba.ux.MessageBox', {
+    override: 'Ext.MessageBox',
+
+    alert: function(title, message, fn, scope) {
+        Ext.device.Notification.show({
+            title: title == null ? 'Atenção' : title,
+            message: message,
+            buttons: Ext.MessageBox.OK,
+            callback: fn,
+            scope: scope
+        });
+    },
+
+    confirm: function(title, message, fn, scope) {
+        Ext.device.Notification.show({
+            title: title == null ? 'Atenção' : title,
+            message: message,
+            buttons: Ext.MessageBox.YESNO,
+            callback: fn,
+            scope: scope
+        });
+    }
+});
